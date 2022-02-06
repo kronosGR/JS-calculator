31932da85f94c2372f436d6a3ce0a89d1870365b
KronosGR <geo.elgeo@gmail.com>
2022-02-06 20:08:37 +0100

    added reset and decimal functionality

--- script.js
+++ script.js
@@ -7,12 +7,24 @@ function sendNumberValue(number) {
   calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal(){
+  if (!calculatorDisplay.textContent.includes('.')){
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+  }
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
-    inputBtn.addEventListener('click', () => sendNumberValue());
+    inputBtn.addEventListener('click', () => addDecimal());
   }
 });
+
+function resetAll(){
+  calculatorDisplay.textContent = '0'
+}
+
+clearBtn.addEventListener('click', resetAll)
