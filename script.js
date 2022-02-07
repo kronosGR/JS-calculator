7028b9c0e3f627e9860f42b0a747feda6ac657b3
KronosGR <geo.elgeo@gmail.com>
2022-02-07 12:55:16 +0100

    added operator functionality

--- script.js
+++ script.js
@@ -2,29 +2,52 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+  }
 }
 
-function addDecimal(){
-  if (!calculatorDisplay.textContent.includes('.')){
-    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+function addDecimal() {
+  if (awaitingNextValue) return;
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+  }
+  awaitingNextValue = true;
+  operatorValue = operator;
 }
 
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
     inputBtn.addEventListener('click', () => addDecimal());
   }
 });
 
-function resetAll(){
-  calculatorDisplay.textContent = '0'
+function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitingNextValue = false;
+  calculatorDisplay.textContent = '0';
 }
 
-clearBtn.addEventListener('click', resetAll)
+clearBtn.addEventListener('click', resetAll);
